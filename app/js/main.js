e013fbc34161cc6afaaf0f70c39f48e0667462df
Shestakovandrij <shestakovandrij@gmai.com>
2021-01-29 10:32:35 +0100

    fix

--- app/js/main.js
+++ app/js/main.js
@@ -1,13 +1,3 @@
 $(function(){
 
-  $(".star").rateYo({
-    rating: 5,
-    starWidth: "12px",
-    ratedFill: "rgba(139, 108, 202, 1)"
-  });
-
-  $('.menu__btn').on('click', function(){
-    $('.header__top').toggleClass('header__top--active')
-  });
-
 });
