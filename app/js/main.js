6c7507ad2092afcbb66e4ca3e6408afe2c0ef918
Shestakovandrij <shestakovandrij@gmai.com>
2021-02-09 15:37:56 +0100

    fix

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,31 @@
 $(function(){
 
+  $(".menu a, .header a").on("click", function (e) {
+		  e.preventDefault();
+		  var id  = $(this).attr('href'),
+			top = $(id).offset().top;
+		  $('body,html').animate({scrollTop: top}, 1000);
+	 });
+
+   function scrollMenu() {
+    let menu = document.querySelector('.menu');
+    window.addEventListener('scroll', () => {
+      if (pageYOffset >= document.documentElement.clientHeight / 1.5) {
+        menu.style['position'] = 'fixed'
+        menu.style['top'] = '0'
+        menu.style['width'] = '100%'
+        menu.style['z-index'] = '999'
+      } 
+    });
+  }
+  scrollMenu();
+
+  $(".footer__btn").on("click touchstart", function () {
+    $(this).addClass("fly");
+    that = this
+    setTimeout(function() {
+        $(that).removeClass("fly");
+    }, 5400)
+});
+
 });
